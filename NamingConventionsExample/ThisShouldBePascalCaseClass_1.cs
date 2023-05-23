77e839b7391a3786980b1581b0432b9b860da6db
Artem Koloskov <artem@ironsoftware.com>
2023-05-23 14:23:19 +0700

    Add project files.

---
+++ NamingConventionsExample/ThisShouldBePascalCaseClass_1.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisShouldBePascalCaseNamespace
+{
+    internal interface IThisShouldBeIPascalCaseInterface
+    {
+        void ThisShouldBePascalCaseMethod(string thisShouldBeCamelCase, int thisShouldBeCamelCaseToo);
+    }
+
+    internal class ThisShouldBePascalCaseClass
+    {
+        public const string THIS_SHOULD_BE_ALL_CAPS_WITH_UNDERSORES = "fileName";
+        private const string THIS_SHOULD_BE_ALL_CAPS_WITH_UNDERSORES_2 = "fileName2";
+        internal const string THIS_SHOULD_BE_ALL_CAPS_WITH_UNDERSORES_3 = "fileName3";
+        public static string s_ThisShouldBeSUndersocrePascalCase = "fileName4";
+        private static readonly string s_thisShouldBeSUndersocreCamelCase = "fileName5";
+        internal static string s_thisShouldBeSUndersocreCamelCase2 = "fileName6";
+        public string ThisShouldBePascalCase = "fileName7";
+        private readonly string _thisShouldBeUnderscoreCamelCase = "fileName8";
+        internal string _thisShouldBeUnderscoreCamelCase2 = "fileName9";
+
+        public string ThisShouldBePascalCase2
+        {
+            get
+            {
+                return "fileName10";
+            }
+            set
+            {
+                _thisShouldBeUnderscoreCamelCase2 = value;
+            }
+        }
+
+        private string _thisShouldBeUnderscoreCamelCase3
+        {
+            get
+            {
+                return "fileName11";
+            }
+            set
+            {
+                _thisShouldBeUnderscoreCamelCase2 = value;
+            }
+        }
+
+        internal string _thisShouldBeUnderscoreCamelCase4
+        {
+            get
+            {
+                return "fileName12";
+            }
+            set
+            {
+                _thisShouldBeUnderscoreCamelCase2 = value;
+            }
+        }
+
+        public static string s_ThisShouldbeSUndersocrePascalCase { get; set; } = "fileName13";
+
+        public ThisShouldBePascalCaseClass()
+        {
+            Console.WriteLine(THIS_SHOULD_BE_ALL_CAPS_WITH_UNDERSORES);
+            Console.WriteLine(THIS_SHOULD_BE_ALL_CAPS_WITH_UNDERSORES_2);
+            Console.WriteLine(THIS_SHOULD_BE_ALL_CAPS_WITH_UNDERSORES_3);
+            Console.WriteLine(s_thisShouldBeSUndersocreCamelCase);
+            Console.WriteLine(s_thisShouldBeSUndersocreCamelCase2);
+            Console.WriteLine(ThisShouldBePascalCase);
+            Console.WriteLine(_thisShouldBeUnderscoreCamelCase);
+            Console.WriteLine(_thisShouldBeUnderscoreCamelCase2);
+            Console.WriteLine(ThisShouldBePascalCase2);
+            Console.WriteLine(_thisShouldBeUnderscoreCamelCase3);
+            Console.WriteLine(_thisShouldBeUnderscoreCamelCase4);
+            Console.WriteLine(s_ThisShouldbeSUndersocrePascalCase);
+            ThisShouldBePascalCaseMethod2();
+            _ = ThisShouldBePascalCaseAsyncMethodAsync();
+        }
+
+        private static async Task ThisShouldBePascalCaseAsyncMethodAsync()
+        {
+            await Task.Delay(0);
+        }
+
+        public static void ThisShouldBePascalCaseMethod(string thisShouldBeCamelCase, int thisShouldBeCamelCaseToo)
+        {
+            _ = thisShouldBeCamelCase + thisShouldBeCamelCaseToo;
+            var thisShouldBeCamelCase3 = thisShouldBeCamelCase + thisShouldBeCamelCaseToo;
+            Console.WriteLine(thisShouldBeCamelCase3);
+        }
+
+        private static void ThisShouldBePascalCaseMethod2()
+        {
+
+        }
+    }
+
+    public enum ThisShouldBePascalCaseEnum2
+    {
+        ThisShouldBePascalCaseEnumEntry = 0
+    }
+}
---
+++ NamingConventionsExample/thisShouldBePascalCaseClass.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thisShouldBePascalCaseNamespace
+{
+    internal interface thisShouldBeIPascalCaseInterface
+    {
+        void thisShouldBePascalCaseMethod(string ThisShouldBeCamelCase, int ThisShouldBeCamelCaseToo);
+    }
+
+    internal class thisShouldBePascalCaseClass
+    {
+        public const string thisShouldBeAllCapsWithUndersores = "fileName";
+        private const string thisShouldBeAllCapsWithUndersores2 = "fileName2";
+        internal const string thisShouldBeAllCapsWithUndersores3 = "fileName3";
+        public static string thisShouldBeSUndersocrePascalCase = "fileName4";
+        private static readonly string thisShouldBeSUndersocreCamelCase = "fileName5";
+        internal static string thisShouldBeSUndersocreCamelCase2 = "fileName6";
+        public string thisShouldBePascalCase = "fileName7";
+        private readonly string thisShouldBeUnderscoreCamelCase = "fileName8";
+        internal string thisShouldBeUnderscoreCamelCase2 = "fileName9";
+
+        public string thisShouldBePascalCase2
+        {
+            get
+            {
+                return "fileName10";
+            }
+            set
+            {
+                thisShouldBeUnderscoreCamelCase2 = value;
+            }
+        }
+
+        private string thisShouldBeUnderscoreCamelCase3
+        {
+            get
+            {
+                return "fileName11";
+            }
+            set
+            {
+                thisShouldBeUnderscoreCamelCase2 = value;
+            }
+        }
+
+        internal string thisShouldBeUnderscoreCamelCase4
+        {
+            get
+            {
+                return "fileName12";
+            }
+            set
+            {
+                thisShouldBeUnderscoreCamelCase2 = value;
+            }
+        }
+
+        public static string thisShouldbeSUndersocrePascalCase { get; set; } = "fileName13";
+
+        public thisShouldBePascalCaseClass()
+        {
+            Console.WriteLine(thisShouldBeAllCapsWithUndersores);
+            Console.WriteLine(thisShouldBeAllCapsWithUndersores2);
+            Console.WriteLine(thisShouldBeAllCapsWithUndersores3);
+            Console.WriteLine(thisShouldBeSUndersocreCamelCase);
+            Console.WriteLine(thisShouldBeSUndersocreCamelCase2);
+            Console.WriteLine(thisShouldBePascalCase);
+            Console.WriteLine(thisShouldBeUnderscoreCamelCase);
+            Console.WriteLine(thisShouldBeUnderscoreCamelCase2);
+            Console.WriteLine(thisShouldBePascalCase2);
+            Console.WriteLine(thisShouldBeUnderscoreCamelCase3);
+            Console.WriteLine(thisShouldBeUnderscoreCamelCase4);
+            Console.WriteLine(thisShouldbeSUndersocrePascalCase);
+            thisShouldBePascalCaseMethod2();
+            _ = thisShouldBePascalCaseAsyncMethod();
+        }
+
+        private static async Task thisShouldBePascalCaseAsyncMethod()
+        {
+            await Task.Delay(0);
+        }
+
+        public static void thisShouldBePascalCaseMethod(string ThisShouldBeCamelCase, int ThisShouldBeCamelCaseToo)
+        {
+            _ = ThisShouldBeCamelCase + ThisShouldBeCamelCaseToo;
+            var ThisShouldBeCamelCase3 = ThisShouldBeCamelCase + ThisShouldBeCamelCaseToo;
+            Console.WriteLine(ThisShouldBeCamelCase3);
+        }
+
+        private static void thisShouldBePascalCaseMethod2()
+        {
+
+        }
+    }
+
+    public enum thisShouldBePascalCaseEnum
+    {
+        thisShouldBePascalCaseEnumEntry = 0
+    }
+}
